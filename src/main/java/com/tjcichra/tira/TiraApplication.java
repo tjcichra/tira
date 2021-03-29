2a28066c326f17c686baa719c04d10e7a3ae9e14
tjcichra <8120075+tjcichra@users.noreply.github.com>
2021-03-29 18:57:55 -0400

    Added Spring Boot

---
+++ src/main/java/com/tjcichra/tira/TiraApplication.java
@@ -0,0 +1,13 @@
+package com.tjcichra.tira;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tjcichra/tira/TiraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjcichra.tira;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
