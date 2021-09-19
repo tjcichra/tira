948bcbd6bb81c26e14011e62263a51362cd5fb87
tjcichra <8120075+tjcichra@users.noreply.github.com>
2021-09-19 11:57:01 -0400

    Added tira-api-gateway

---
+++ tira-api-gateway/src/main/java/com/tjcichra/tiraapigateway/TiraApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.tjcichra.tiraapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiraApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiraApiGatewayApplication.class, args);
+	}
+
+}
---
+++ tira-api-gateway/src/test/java/com/tjcichra/tiraapigateway/TiraApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tjcichra.tiraapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiraApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
